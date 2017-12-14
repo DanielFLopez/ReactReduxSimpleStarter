e68e36f3c58d9f9dc4353a9c367ca54b7aa658a0
DanielFLopez <daniel.f.lopez.j@gmail.com>
2017-12-13 21:58:08 -0500

    making a search bar

---
+++ ReduxSimpleStarter/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyC9SCtac_VmbHXqfA2Prso6IrWtXhU5UmY';
+
+const App = () => {
+  return (<div>
+    <SearchBar />
+  </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
