da81da9aaaa819231900a867c8eae07bd87f41c3
DanielFLopez <daniel.f.lopez.j@gmail.com>
2017-12-13 22:44:12 -0500

    handling events

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -6,9 +6,10 @@ import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyC9SCtac_VmbHXqfA2Prso6IrWtXhU5UmY';
 
 const App = () => {
-  return (<div>
-    <SearchBar />
-  </div>
+  return (
+    <div>
+      <SearchBar />
+    </div>
   );
 }
 
